a78a18b273ecf94c343fec0864bd2b35224fda53
Sidhant Mishra <Sidhantmishra101@gmail.com>
2024-07-07 01:23:37 +0530

    final

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>TRENDIFY</p>
+        <p>SHOPPER</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt="" />
-        <p>TRENDIFY</p>
+        <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
